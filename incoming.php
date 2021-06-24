b1228c23b7488b701ffce594eb281463ec58675f
jt.boron <JT.BORON@GMAIL.COM>
2021-06-24 07:29:50 -0400

    just starting

---
+++ incoming.php
@@ -0,0 +1,9 @@
+<?php
+header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS'); 
+header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+
+
+
+
+?>
